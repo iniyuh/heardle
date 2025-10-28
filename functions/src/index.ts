5113f0917826923b9f74620ac0d6e19c6038e135
Iniyan <iniyuh@proton.me>
2025-10-28 07:07:11 -0400

    setup functions

---
+++ functions/src/index.ts
@@ -0,0 +1,21 @@
+import {onRequest} from "firebase-functions/https";
+import * as logger from "firebase-functions/logger";
+// import { onDocumentCreated } from "firebase-functions/firestore";
+import { getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+if (getApps().length === 0) initializeApp();
+
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const addMessage = onRequest(async (req, res) => {
+    const original = req.query.text;
+
+    const writeResult = await getFirestore().collection('testMessages').add({original: original});
+    
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
