9385cb5d12c61e887546a8a490887df553a4ad38
Iniyan <iniyuh@proton.me>
2025-10-29 04:39:29 -0400

    initial setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 // import { onDocumentCreated } from "firebase-functions/firestore";
-import { getApps, initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import {getApps, initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
 if (getApps().length === 0) initializeApp();
 
@@ -13,9 +13,11 @@ export const helloWorld = onRequest((request, response) => {
 });
 
 export const addMessage = onRequest(async (req, res) => {
-    const original = req.query.text;
+  const original = req.query.text;
 
-    const writeResult = await getFirestore().collection('testMessages').add({original: original});
-    
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  const writeResult = await getFirestore()
+    .collection("testMessages")
+    .add({original: original});
+
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
